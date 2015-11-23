c8daf34787273d923e84da6767653cb0a29bc0b8
nexon <nexon@owe>
2015-11-23 21:36:16 +0100

    Bakoffice admin ajout projet ok

--- src/Ingesup/MonteChargeBundle/Controller/UserController.php
+++ src/Ingesup/MonteChargeBundle/Controller/UserController.php
@@ -7,7 +7,7 @@ use Ingesup\MonteChargeBundle\Entity\Projet;
 use Ingesup\MonteChargeBundle\Entity\Type;
 use Ingesup\MonteChargeBundle\Entity\Classe;
 use Ingesup\MonteChargeBundle\Entity\Groups;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,7 +41,6 @@ class UserController extends CoreController
     }
     public function overrideDashboardAction()
     {
-
         $blocks = array(
             'top'    => array(),
             'left'   => array(),
@@ -55,15 +54,73 @@ class UserController extends CoreController
         }
 
         $em = $this->getDoctrine()->getManager();
+
         $projects = $em->getRepository('IngesupMonteChargeBundle:Projet')->findAll();
         $types = $em->getRepository('IngesupMonteChargeBundle:Type')->findAll();
+        $difficulties = $em->getRepository('IngesupMonteChargeBundle:Difficulty')->findAll();
+        $classes = $em->getRepository('IngesupMonteChargeBundle:Classe')->findAll();
+        $groups = $em->getRepository('IngesupMonteChargeBundle:Groups')->findAll();
 
         return $this->render($this->getAdminPool()->getTemplate('dashboard'), array(
             'projects'        => $projects,
             'types'           => $types,
+            'difficulties'    => $difficulties,
+            'classes'         => $classes,
+            'groups'          => $groups,
             'base_template'   => $this->getBaseTemplate(),
             'admin_pool'      => $this->container->get('sonata.admin.pool'),
             'blocks'          => $blocks,
         ));
+    }
+    public function newUserDashBoardAction(Request $request){
+
+
+        if ($request->isMethod('POST')) {
+            $_name       = $request->request->get('name');
+            $_manager    = $request->request->get('manager');
+            $_type       = $request->request->get('type');
+            $_classe     = $request->request->get('classe');
+            $_group      = $request->request->get('group' );
+            $_difficulty = $request->request->get('difficulty');
+            $_startdate  = $request->request->get('startdate');
+            $_deadline   = $request->request->get('deadline');
+
+            $_url        = $request->request->get('url');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $type = $em->getRepository('IngesupMonteChargeBundle:Type')->find((int)$_type);
+        $difficulty = $em->getRepository('IngesupMonteChargeBundle:Difficulty')->find((int)$_difficulty);
+        $classe = $em->getRepository('IngesupMonteChargeBundle:Classe')->find((int)$_classe);
+        $group = $em->getRepository('IngesupMonteChargeBundle:Groups')->find((int)$_group);
+
+        $projet = new Projet;
+
+
+        $type->setDifficulty($difficulty);
+
+        $projet->setName($_name)
+            ->setManager($_manager)
+            ->setClasse($classe)
+            ->setUrl($_url);
+
+
+            $type->setDifficulty($difficulty);
+            $projet->setType($type);
+
+
+            $projet->setGroups($group);
+
+            $projet->setStartdate(\DateTime::createFromFormat('d/m/Y H:i:s', $_startdate));
+            $projet->setDeadline(\DateTime::createFromFormat('d/m/Y H:i:s', $_deadline));
+
+            $em->persist($projet);
+
+            $em->flush();
+
+
+        return new RedirectResponse($this->generateUrl('sonata_admin_dashboard'));
+
     }
 }
